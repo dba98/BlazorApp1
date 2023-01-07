a6b7f84272dde40b8744f0bc7f80e8cac8d983df
Diogo Araújo <dg.borges1@gmail.com>
2023-01-07 16:03:02 +0000

    Interact with data in Blazor web apps module completed

---
+++ BlazorApp1/Data/PizzaStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+} 
